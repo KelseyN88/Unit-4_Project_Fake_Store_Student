154ebe557251d155813b3389111256f42def7669
Kelsey Nagy <nagy8907@gmail.com>
2023-07-10 23:43:24 -0400

    U4_04 working on retreiving data & displaying it

--- main.js
+++ main.js
@@ -8,12 +8,15 @@ const mensClothing = document.querySelectorAll('.nav-link')[3]
 const womensClothing = document.querySelectorAll('.nav-link')[4]
 
 const displayDiv = document.getElementById('display')
+let storedItems = []
 
 // Fake Store Arrow Function
 const fakeStore = async (endpoint) => {
     const response = await fetch(baseURL + endpoint)
     const data = await response.json()
+    storedItems = data
     console.log(data);
+    displayCards()
 };
 
 // Event Listeners for navbar
@@ -38,7 +41,124 @@ womensClothing.addEventListener('click', () => {
 })
 
 
+const displayCards = () => {
 
+storedItems.forEach(item => {
+
+// Create elements
+const displayDiv = document.createElement('div');
+const innerDiv = document.createElement('div');
+const columnDiv = document.createElement('div');
+
+const cardDiv = document.createElement('div');
+const cardImage = document.createElement('img');
+const cardBodyDiv = document.createElement('div');
+const cardTitle = document.createElement('h5');
+const cardText = document.createElement('p');
+
+const accordionDiv = document.createElement('div');
+const accordionItem1 = document.createElement('div');
+const accordionHeader1 = document.createElement('h2');
+const accordionButton1 = document.createElement('button');
+const accordionCollapse1 = document.createElement('div');
+const accordionBody1 = document.createElement('div');
+
+const accordionDiv2 = document.createElement('div');
+const accordionItem2 = document.createElement('div');
+const accordionHeader2 = document.createElement('h2');
+const accordionButton2 = document.createElement('button');
+const accordionCollapse2 = document.createElement('div');
+const accordionBody2 = document.createElement('div');
+
+const placeholderText1 = document.createElement('h3');
+const placeholderText2 = document.createElement('h3');
+
+const addToCartButton = document.createElement('button');
+
+
+// Set Attributes
+displayDiv.id = 'display';
+// found .classList and it seemed helpful for manipulating classes easily, especially for accordian
+innerDiv.classList.add('row', 'row-cols-1', 'row-cols-md-2', 'g-4');
+innerDiv.style.maxWidth = '36rem';
+columnDiv.classList.add('col');
+
+cardDiv.classList.add('card');
+cardImage.classList.add('card-img-top');
+cardImage.alt = `A photo of ${item.image}`;
+cardBodyDiv.classList.add('card-body');
+cardTitle.classList.add('card-title');
+cardText.classList.add('card-text');
+
+accordionDiv.classList.add('accordion');
+accordionDiv.id = 'accordionExample';
+accordionItem1.classList.add('accordion-item');
+accordionHeader1.classList.add('accordion-header');
+accordionButton1.classList.add('accordion-button', 'collapsed');
+accordionButton1.type = 'button';
+accordionButton1.setAttribute('data-bs-toggle', 'collapse');
+accordionButton1.setAttribute('aria-expanded', 'false');
+accordionCollapse1.classList.add('accordion-collapse', 'collapse');
+accordionCollapse1.setAttribute('data-bs-parent', '#accordionExample');
+accordionBody1.classList.add('accordion-body');
+
+accordionItem2.classList.add('accordion-item');
+accordionHeader2.classList.add('accordion-header');
+accordionButton2.classList.add('accordion-button', 'collapsed');
+accordionButton2.type = 'button';
+accordionButton2.setAttribute('data-bs-toggle', 'collapse');
+accordionButton2.setAttribute('aria-expanded', 'false');
+accordionCollapse2.classList.add('accordion-collapse', 'collapse');
+accordionCollapse2.setAttribute('data-bs-parent', '#accordionExample');
+accordionBody2.classList.add('accordion-body');
+
+addToCartButton.classList.add('btn', 'btn-text');
+addToCartButton.textContent = 'Add To Cart';
+
+cardImage.src = item.image;
+cardTitle.textContent = item.title;
+cardText.textContent = item.description;
+placeholderText1.textContent = 'Placeholder Text';
+placeholderText2.textContent = 'Placeholder Text';
+
+accordionButton1.textContent = 'Description';
+accordionButton1.setAttribute('data-bs-target', `#collapse${item.id}`);
+accordionButton1.setAttribute('aria-controls', `collapse${item.id}`);
+accordionCollapse1.id = `collapse${item.id}`;
+accordionBody1.appendChild(placeholderText1);
+accordionCollapse1.appendChild(accordionBody1);
+accordionHeader1.appendChild(accordionButton1);
+accordionItem1.appendChild(accordionHeader1);
+accordionItem1.appendChild(accordionCollapse1);
+
+accordionButton2.textContent = 'Price';
+accordionButton2.setAttribute('data-bs-target', `#collapse${item.id + 1}`);
+accordionButton2.setAttribute('aria-controls', `collapse${item.id + 1}`);
+
+// Append Elements
+accordionCollapse2.id = `collapse${item.id + 1}`;
+accordionBody2.appendChild(placeholderText2);
+accordionCollapse2.appendChild(accordionBody2);
+accordionHeader2.appendChild(accordionButton2);
+accordionItem2.appendChild(accordionHeader2);
+accordionItem2.appendChild(accordionCollapse2);
+
+accordionDiv.appendChild(accordionItem1);
+accordionDiv.appendChild(accordionItem2);
+
+cardBodyDiv.appendChild(cardTitle);
+cardBodyDiv.appendChild(cardText);
+cardBodyDiv.appendChild(accordionDiv);
+cardBodyDiv.appendChild(addToCartButton);
+cardDiv.appendChild(cardImage);
+cardDiv.appendChild(cardBodyDiv);
+columnDiv.appendChild(cardDiv);
+innerDiv.appendChild(columnDiv);
+displayDiv.appendChild(innerDiv);
+document.body.appendChild(displayDiv);
+})
+
+}
 
 
 
